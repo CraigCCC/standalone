1aad826557e05400e9ff7c095332ff39eeb95ced
craig <craigya89@gmail.com>
2020-03-09 19:09:15 +0800

    Product Controller - Generate controller - Findish basic CRUD(params_product WIP)

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,50 @@
+class ProductsController < ApplicationController
+  before_action :find_product, only[:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params_product)
+
+    if @product
+      redirect_to products_path, notice: '新增產品成功'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(params_product)
+      redirect_to edit_product_path, notice: '更新產品成功'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @produc.destroy
+    redirect_to products_path, notice: '刪除產品成功'
+  end
+
+  private
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def params_product
+    # params.require(:product).permit([:])
+  end
+end
